69089bea43f73a2739931821e3612e03fbc9f85d
Parth Aggarwal <parthaggarwal2024@gmail.com>
2026-07-19 16:46:50 +0530

    Add configuration variables for API and models

---
+++ config.py
@@ -0,0 +1,7 @@
+EMAIL = "24f3003200@ds.study.iitm.ac.in"
+AIPIPE_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjI0ZjMwMDMyMDBAZHMuc3R1ZHkuaWl0bS5hYy5pbiIsImlhdCI6MTc4NDQ1OTA2NCwiaXNzIjoiaHR0cHM6Ly9haXBpcGUub3JnIiwiYXVkIjoiYWlwaXBlLWFwaSIsImV4cCI6MTc4NTA2Mzg2NH0.ujgfF3D1Ww8xlI2TEpAr93gN_ER7pEUZfc6x-mHIngw"
+
+AIPIPE_BASE = "https://aipipe.org/openai/v1"
+TEXT_MODEL = "gpt-4o-mini"
+VISION_MODEL = "gpt-4o"          
+EMBED_MODEL = "text-embedding-3-small"
