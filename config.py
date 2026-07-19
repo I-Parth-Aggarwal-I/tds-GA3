32a0d8da018e7216375011a87b44996f7953a494
Parth Aggarwal <parthaggarwal2024@gmail.com>
2026-07-19 17:01:52 +0530

    Clear sensitive data from config.py
    Removed sensitive email and token information.

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-EMAIL = "24f3003200@ds.study.iitm.ac.in"
-AIPIPE_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjI0ZjMwMDMyMDBAZHMuc3R1ZHkuaWl0bS5hYy5pbiIsImlhdCI6MTc4NDQ1OTA2NCwiaXNzIjoiaHR0cHM6Ly9haXBpcGUub3JnIiwiYXVkIjoiYWlwaXBlLWFwaSIsImV4cCI6MTc4NTA2Mzg2NH0.ujgfF3D1Ww8xlI2TEpAr93gN_ER7pEUZfc6x-mHIngw"
+EMAIL = ""
+AIPIPE_TOKEN = ""
 
 AIPIPE_BASE = "https://aipipe.org/openai/v1"
 TEXT_MODEL = "gpt-4o-mini"
